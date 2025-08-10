5fd306174f729e358b1aabe30639f476ac15528c
Ahmet Furkan Öcel <157826103+KayzerFurkan04@users.noreply.github.com>
2025-08-10 17:30:04 +0300

    Update
    Tekrar eden kelimelerin, kaçar kez tekrar ettiklerine göre en çok tekrar edenden en az tekrar edene doğru sıralanmış bir biçimde konsola yazılmasını ayarladım. Kodun işleyişini daha iyi anlamak adına satır satır yazdım ve inceledim, ve her işlemde, metot da ne yapıldığını ve nasıl yapıldığını yorum satırları ile açıkladım.

--- FileAnalyzer_Console/TextAnalyzer.cs
+++ FileAnalyzer_Console/TextAnalyzer.cs
@@ -22,7 +22,7 @@ namespace FileAnalyzer_Console
             string[] conjunctions = { "ve", "ile", "ama", "ancak" }; // sayılmayacak bağlaçlar.
             string[] words = content.Split(' ', '\n', '\r'); // boşluk, \n veya \r gördüğünde kelimeyi bitirir.
 
-            List<string> filteredWords = new List<string>(); // dizinin boyutu sabittir, biz burada filtrelenmiş metinin kaç elemanlı olduğunu bilmediğimiz için dinamik boyutlu liste oluşturduk.
+            List<string> filteredWords = new List<string>(); // dizinin boyutu sabittir, biz burada filtrelenmiş metinin kaç elemanlı olduğunu bilmediğimiz için dinamik boyutlu olan liste oluşturduk.
 
             foreach (string word in words) // metin words dizisinin içerisinde kelime kelime tutuluyor, "string word in words" denince bu dizideki her bir eleman yani kelime, string türünde word isimli değişkene tek tek atanıyor. 
             {
@@ -37,10 +37,37 @@ namespace FileAnalyzer_Console
                 }
             }
 
-            int uniqueWordCount = filteredWords.Distinct().Count();  // distinct tekrar eden kelimeleri siler.ile tekrar eden kelimeleri siliyoruz.
+            int uniqueWordCount = filteredWords.Distinct().Count();  // distinct ile tekrar eden kelimeleri siliyoruz.
+
+            // tekrar eden kelimeleri ve bu kelimelerin kaç defa tektrar ettiklerinin sayısını, en çok tekrar edenden en az tekrar edene doğru sıralanmış şekilde konsola yazdıracağımız kısım burası.
+            //-----------------------------------------------------------------
+            Dictionary<string, int> wordAndCounts = new Dictionary<string, int>(); // burada dictionary kullanıyoruz bu sayede kelimeler ve tekrar sayıları bir arada tutabiliyoruz. tekrar eden kelime sayısına göre tekrar sayısı yazdığımız için Dictionary<string, int> yapısını kullandık.
+
+            foreach (var word in filteredWords) // filtrelenmiş kelimelerin bulunduğu listedeki her bir elemanı yani kelimeyi, word isimli olarak döngüye sokuyoruz.
+            {
+                if (wordAndCounts.ContainsKey(word)) // sözlükteki kelime, test ettiğimiz kelimeyle uyuşuyorsa kelimenin Value değerini yani bu sözlükteki int değerini 1 artırıyoruz. Sözlüğün yapısı: Dictionary<TKey, TValue>
+                    wordAndCounts[word]++;
+                else
+                    wordAndCounts[word] = 1; // yoksa tekrar sayısını 1 yapıyoruz çünkü test ettiğimiz kelimeye ilk defa rastladık.
+            }
+
+            var sortedWordCounts = wordAndCounts.OrderByDescending(wordAndValue => wordAndValue.Value); // Tekrar eden kelimeleri büyükten küçüğe sıralamak için sıralama algoritmaları kullanmam gerekicekti. Onun yerine Bu LINQ yapısını kullandım. bir LINQ metodu olan OrderByDescending wordAndCounts daki her bir elemanı wordAndValue ismiyle alıyor ve büyükten küçüğe doğru sıralıyor. ve bu sıralanmış halini sortedWordCounts a atıyoruz.
+            //-------------------------------------------------------------------
+
             Console.WriteLine($"Character count: {charCount}");
             Console.WriteLine($"Line count: {lineCount}");
             Console.WriteLine($"Unique word count: {uniqueWordCount}");
+            Console.WriteLine("");
+            Console.WriteLine("repetitive words");
+            Console.WriteLine("----------------");
+
+            foreach (var repetitiveWords in sortedWordCounts)
+            {
+                if (repetitiveWords.Value > 1) // kelimenin tekrar sayısı 1 ise almıyoruz çünkü mantıken kelime 1 defa geçiyorsa tekrar etmiyordur.
+                {
+                    Console.WriteLine($"{repetitiveWords.Value}  {repetitiveWords.Key}");
+                }
+            }
         }
     }
 }
